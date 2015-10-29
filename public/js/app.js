03c7fb1989e9d6bcec3c9f48a6a973907e90fd6b
Anton Bardishev <abar193@gmail.com>
2015-10-30 00:16:14 +0100

    Hotfix for system bug

--- public/js/app.js
+++ public/js/app.js
@@ -60,12 +60,13 @@ conkingsui.directive('sectorCanvas', function() {
                 for(var i = 0; i < tunnels.length; i++) {
                     var cFrom = locToCoordinates(tunnels[i].from);
                     var cTo = locToCoordinates(tunnels[i].to);
-
-                    if(cFrom.sector == sector || cTo.sector == sector) {
-                        ctx.moveTo((cFrom.sector == sector) ? cFrom.x * 21 + 31 : (cFrom.sector > sector) ? 651 : 0,
-                            (cFrom.sector == sector) ? cFrom.y * 21 + 31 : cTo.y * 21 + 35);
-                        ctx.lineTo((cTo.sector == sector) ? cTo.x * 21 + 31 : (cTo.sector > sector) ? 651 : 0,
-                            (cTo.sector == sector) ? cTo.y * 21 + 31 : cFrom.y * 21 + 35);
+                    if(cTo && cFrom) {
+                        if(cFrom.sector == sector || cTo.sector == sector) {
+                            ctx.moveTo((cFrom.sector == sector) ? cFrom.x * 21 + 31 : (cFrom.sector > sector) ? 651 : 0,
+                                (cFrom.sector == sector) ? cFrom.y * 21 + 31 : cTo.y * 21 + 35);
+                            ctx.lineTo((cTo.sector == sector) ? cTo.x * 21 + 31 : (cTo.sector > sector) ? 651 : 0,
+                                (cTo.sector == sector) ? cTo.y * 21 + 31 : cFrom.y * 21 + 35);
+                        }
                     }
                 }
                 ctx.strokeStyle="#3333BB";
